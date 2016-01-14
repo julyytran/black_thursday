69704d91e5bc64bdd32ba946a030a1f7bc2b26ec
July Tran <julyytran@gmail.com>
2016-01-13 18:51:29 -0700

    Made fixtures and refactored tests

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -4,51 +4,39 @@ require_relative '../lib/sales_engine'
 
 class SalesEngineTest < Minitest::Test
 
+  attr_reader :mr, :ir
+
+  def setup
+    @se = SalesEngine.from_csv({
+      :items    => './data/fixtures/items.csv',
+      :merchants => './data/fixtures/merchants.csv'})
+    @mr = @se.merchants
+    @ir = @se.items
+  end
+
   def test_loads_data_into_repositories
-    se = SalesEngine.from_csv({
-      :items    => './data/items.csv',
-      :merchants => './data/merchants.csv'})
-    mr = se.merchants
-    ir = se.items
     refute mr.all.empty?
     refute ir.all.empty?
   end
 
   def test_returns_merchant_id
-    se = SalesEngine.from_csv({
-      :items    => './data/items.csv',
-      :merchants => './data/merchants.csv'})
-    mr = se.merchants
     merchant_1 = mr.find_by_id("12334105")
-
     assert_equal "12334105", merchant_1.id
   end
 
   def test_returns_item_id
-    se = SalesEngine.from_csv({
-      :items    => './data/items.csv',
-      :merchants => './data/merchants.csv'})
-    ir = se.items
     item_1 = ir.find_by_id("263395237")
-
     assert_equal "263395237", item_1.id
   end
 
   def test_returns_all_items_that_match_a_merchants_id
-    se = SalesEngine.from_csv({
-      :items    => './data/items.csv',
-      :merchants => './data/merchants.csv'})
-    merchant = se.merchants.find_by_id("12334105")
+    merchant = mr.find_by_id("12334105")
     merchant_items = merchant.items
-    assert_equal 3, merchant_items.count
+    assert_equal 1, merchant_items.count
     end
 
     def test_returns_merchant_that_match_item_id
-      se = SalesEngine.from_csv({
-        :items    => './data/items.csv',
-        :merchants => './data/merchants.csv'})
-      item = se.items.find_by_id("263395237")
-
+      item = ir.find_by_id("263395237")
       assert_equal '12334141', item.merchant.id
     end
 
