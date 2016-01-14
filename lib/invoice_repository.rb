0459fe0607f2e95ee2013778023fd7a81694c023
Nathan Venn <natevenn@gmail.com>
2016-01-14 10:14:48 -0700

    create invoice repo and invoice class

---
+++ lib/invoice_repository.rb
@@ -0,0 +1,34 @@
+require 'csv'
+require_relative 'invoice'
+
+
+class InvoiceRepository
+  attr_reader :all
+
+  def initialize(data)
+    content = CSV.open "#{data}", headers: true, header_converters: :symbol
+    @all = content.map { |row| Invoice.new(row.to_h) }
+  end
+
+  def find_by_id(id)
+    all.detect do |invoice|
+      invoice.id == id
+  end
+
+  def find_all_by_customer_id(customer_id)
+    all.select do |invoice|
+      
+    # returns either [] or one or more matches which have a matching customer ID
+
+  end
+
+  def find_all_by_merchant_id
+    # returns either [] or one or more matches which have a matching merchant ID
+
+  end
+
+  def find_all_by_status
+    # returns either [] or one or more matches which have a matching status
+
+  end
+end
